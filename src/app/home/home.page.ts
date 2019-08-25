0487bc90f66ae721ae84646f01f675a9caf2c44c
HizkiaMumu <hizkiamumu24@gmail.com>
2019-08-26 00:08:49 +0800

    create get books function

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { NavController } from '@ionic/angular';
+import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +10,23 @@ import { NavController } from '@ionic/angular';
 })
 export class HomePage {
 
-  constructor(private navCtrl: NavController) {}
+  token: any;
+
+  constructor(private navCtrl: NavController, private storage: Storage, private http: HttpClient) {
+    this.storage.get('token').then((val) => {
+      this.token = val;
+      this.getBooks();
+    });
+  }
+
+  getBooks(){
+    const headers = new HttpHeaders().set("Authorization", "Bearer " + this.token);
+    this.http.get('http://192.168.1.6:8000/api/get-books', {headers}).subscribe((response) => {
+      console.log(response);
+    }, (err) => {
+      console.log(err);
+    });
+  }
 
   pushDetailBook(){
     this.navCtrl.navigateForward('/detail-book');
